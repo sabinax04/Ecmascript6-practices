f76ecaf5d0cedc0b5083f077b0e39e1690a121a2
Sabina Ibrahimli <sabinetta@Sabinas-Air.STELFIBER>
2024-02-08 20:27:02 +0400

    New things

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // Rest/Spread Operator
 
-let user = { name : "Sabina", age : 20, city : "Baku" };
+//let user = { name : "Sabina", age : 20, city : "Baku" };
 
 //console.log(user);
 
-user = { ...user, city : "Norway" };
+//user = { ...user, city : "Norway" };
 
-console.log(user);
+//console.log(user);
+
+//function add(number1, number2, number3){
+//    return number1 + number2 + number3;
+//}
+
+const numbers = [5,10,15];
+const numbers2 = [20,25,30];
+
+let attachArray = [...numbers, ...numbers2]
+
+console.log(attachArray);
+
+let attachArray2 = numbers.concat(numbers2);
+
+console.log(attachArray2);
+
