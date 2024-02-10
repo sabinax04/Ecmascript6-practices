4bbf4fa09f26fada12d72aa6723f3b1697757278
Sabina Ibrahimli <sabinetta@Sabinas-MacBook-Air.local>
2024-02-10 13:49:36 +0400

    ESmodules

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-// .find() .some() .every() methods
+// ESmodules
 
-const urunler = [
+//import { name, user, hello } from "./export";
 
-    { id : 1, name: "Kalem", fiyat: 5 },
-    { id : 2, name: "Defter", fiyat: 10 },
-    { id : 3, name: "Silgi", fiyat: 2 },
-    { id : 4, name: "Kalemtraş", fiyat: 7 },
-    
-];
-
-console.log("Find : ", urunler.find((urun)=> urun.fiyat > 5));
-
-console.log("Some : ", urunler.some((urun)=> urun.fiyat < 2));
-
-console.log("Every : ", urunler.every((urun)=> urun.fiyat > 1));
+import { default as hello } from "./export"
---
+++ export/export.js
@@ -0,0 +1,10 @@
+//export const name = "Alina";
+//export const user = { name, age: 2 };
+
+
+export default function hello(name){
+    return `Hello, ${name}`;
+}
+
+ 
+//? export { hello as default }
