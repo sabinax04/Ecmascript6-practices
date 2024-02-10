65bbf74eedea6dd44d40c97d484c20c5e94c849c
Sabina Ibrahimli <sabinetta@Sabinas-MacBook-Air.local>
2024-02-10 14:28:46 +0400

    includes and toLowerCase

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-// ESmodules
+// .includes() , .toLowerCase()
 
-//import { name, user, hello } from "./export";
+const name = "Alina";
 
-import { default as hello } from "./export"
+const name2 = "Ravin";
+
+console.log(name.includes("Ali")); // true
+console.log(name.includes("alina")); // false
+
+const newName = name.toLowerCase();
+console.log(newName);
+
+console.log(name.toLowerCase().includes("na"));
+
+console.log(name.toLowerCase().includes(name2.toLowerCase()));
+
+
+const urunler = [
+
+    { id : 1, name: "Kalem", fiyat: 5 },
+    { id : 2, name: "Defter", fiyat: 10 },
+    { id : 3, name: "Silgi", fiyat: 2 },
+    { id : 4, name: "Kalemtraş", fiyat: 7 },
+    
+];
+
+console.log(urunler.filter((urun)=>urun.name.toLowerCase().includes("kalem")));
